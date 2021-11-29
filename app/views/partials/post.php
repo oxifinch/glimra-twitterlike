53ebfd09c001cdbaaac681f84f8c15c04eb8a9ed
Martin WEB. Larsson <20martin.larsson@glimnet.se>
2021-11-29 12:27:57 +0100

    feat: add partial post.php

---
+++ app/views/partials/post.php
@@ -0,0 +1,21 @@
+<section>
+    <div>
+        <ul>
+            <!-- Make these into buttons? -->
+            <!-- TODO make these into templates..! -->
+            <li>Thumbs: 356</li>
+            <li>Smile: 13</li>
+            <li>laugh 123k</li>
+            <li>Glimma-leaf: 14</li>
+            <li>
+                <img src="#" alt="" width="100" height="100">
+                <button>Settings</button>
+            </li>
+        </ul>
+        <img src="#" alt="" width="300" height="300">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, labore esse? Incidunt distinctio minima
+            deleniti eaque quas</p>
+    </div>
+    <button>Read more</button>
+    <small>Posted on 12/12/2001</small>
+</section>
