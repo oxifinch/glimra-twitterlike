1d56d209b58f3ced5941f0ee1c4ff7bc68a3348d
Martin WEB. Larsson <20martin.larsson@glimnet.se>
2021-11-29 12:28:09 +0100

    feat: add partial profile.php

---
+++ app/views/partials/profile.php
@@ -0,0 +1,43 @@
+<section>
+    <header>
+        <img src="#" alt="" width="100" height="100">
+        <ul>
+            <li>
+                <h3>@someone</h3>
+            </li>
+            <li>Mr Someone</li>
+            <li>Shared his gold 12/12/2001</li>
+        </ul>
+        <button>Follow/setting!</button>
+    </header>
+    <div>
+        <h4>Bio</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ab hic, eaque earum architect!</p>
+    </div>
+    <div>
+        <ul>
+            <li>Smile 134</li>
+            <li>Following:</li>
+            <li>
+                <img src="#" alt="" width="100" height="100">
+                <img src="#" alt="" width="100" height="100">
+                <img src="#" alt="" width="100" height="100">
+                <img src="#" alt="" width="100" height="100">
+            </li>
+        </ul>
+        <ul>
+            <li>Masses 123</li>
+            <li>Glimmers</li>
+            <li>
+                <img src="#" alt="" width="100" height="100">
+                <img src="#" alt="" width="100" height="100">
+                <img src="#" alt="" width="100" height="100">
+                <img src="#" alt="" width="100" height="100">
+            </li>
+        </ul>
+        <ul>
+            <li>Leaf 12k</li>
+            <li>Total glimes:</li>
+        </ul>
+    </div>
+</section>
