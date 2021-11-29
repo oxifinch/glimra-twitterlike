1616a5dc13384d7a7ca6258a59738d035ed9cd1a
Martin WEB. Larsson <20martin.larsson@glimnet.se>
2021-11-29 12:28:20 +0100

    feat: add partial header.php

---
+++ app/views/partials/header.php
@@ -0,0 +1,31 @@
+<header>
+    <div>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">My profile</a>
+            </li>
+            <li>
+                <a href="#">Logout</a>
+            </li>
+            <li>
+                <a href="#">Delete Account</a>
+            </li>
+            <li>
+                <a href="#">Edit profile</a>
+            </li>
+            <li>
+                <a href="#">Change password</a>
+            </li>
+        </ul>
+
+    </nav>
+    <form>
+        <input type="text" placeholder="Search after user" name="search" id="navSearch">
+        <button type="submit">Mag</button>
+    </form>
+</header>
