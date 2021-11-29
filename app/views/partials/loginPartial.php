9de1cd1cc734c2f5fe07a15505e64dc1a614579f
Martin WEB. Larsson <20martin.larsson@glimnet.se>
2021-11-29 13:55:53 +0100

    refactor: login partial hass better structure

--- app/views/partials/loginPartial.php
+++ app/views/partials/loginPartial.php
@@ -1,15 +1,23 @@
 <form action="#">
     <h2>Login Glimma</h2>
     <p hidden>You have tried too many times to login, try again in a few minutes </p>
-    <label for="login">
-        <input type="text" id="login" name="#">
-    </label>
-    <small hidden>Username incorrect</small>
-    <label for="register">
-        <input type="text" id="register" name="#">
-    </label>
-    <small hidden>Password incorrect</small>
-
-    <a href="#">Login</a>
-    <a href="register.php">Or register</a>
+    <ul>
+        <li>
+            <label for="login">
+                <input type="text" id="login" name="#">
+            </label>
+            <small hidden>Username incorrect</small>
+        </li>
+        <li>
+            <label for="register">
+                <input type="text" id="register" name="#">
+            </label>
+            <small hidden>Password incorrect</small>
+        </li>
+        <li>
+            <a href="#">Login</a>
+        </li>
+        <li>
+            <a href="register.php">Or register</a>
+        </li>
 </form>
