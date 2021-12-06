62af09373102a1518ca2b6d3c4da104badc0fbd2
Martin WEB. Larsson <20martin.larsson@glimnet.se>
2021-12-06 13:03:20 +0100

    feat: add html skeleton and routes for these

---
+++ app/app/Views/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glimma Twitter</title>
+    <link rel="stylesheet" href="../../public/css/css.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<!-- http://localhost/views/pages/ -->
+
+<body>
+    <h2>Header:</h2>
+    <?=
+$this->include("/partials/header");
+?>
+
+    <h2>Profile:</h2>
+    <?=
+    $this->include("/partials/profile");
+?>
+
+    <h2>Posts:</h2>
+    <?=
+   $this->include("/partials/post");
+?>
+
+    <?=
+   $this->include("/partials/postForm");
+?>
+
+    <div id="loginPartialContainer" class="hidden">
+        <section>
+            <p>You need to log in! <span id="closeLoginPartialContainer">X</span></p>
+            <?= $this->include("/partials/loginPartial"); ?>
+        </section>
+    </div>
+
+    <script src="../../public/js/test.js"></script>
+</body>
+
+</html>
